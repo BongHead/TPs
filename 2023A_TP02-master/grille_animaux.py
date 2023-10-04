b762b07f5f93ba43b2be9f5f93e7c9660fdf7d4d
Bong <songning.lan@gmail.com>
2023-10-03 23:53:17 -0400

    change

--- 2023A_TP02-master/grille_animaux.py
+++ 2023A_TP02-master/grille_animaux.py
@@ -24,50 +24,53 @@ def creer_grille(nb_lignes, nb_colonnes):
 
 def obtenir_population(grille):
     # TODO: Retourner un tuple contenant le nombre actuel de proies et de prédateurs dans la grille (Tuple[Int, Int])
-    pass
+    return grille["nb_proies"], grille["nb_predateurs"]
 
 
 def obtenir_dimensions(grille):
     # TODO: Retourner un tuple avec le nombre de lignes et de colonnes de la grille (Tuple[Int, Int])
-    pass
+    return grille["nb_lignes"], grille["nb_colonnes"]
 
 
-def obtenir_animal(grille, ligne, col):
+def obtenir_animal(grille, ligne, colonne):
     # TODO: Retourner l'animal présent dans la case aux coordonnées données (ligne, col) (Dict)
-    pass
+    return grille["matrice"][ligne][colonne]["animal"]
 
 
 def incrementer_nb_proies(grille):
     # TODO: Augmenter le compteur du nombre de proies dans la grille de 1 (Int)
-    pass
+    grille["nb_proies"] += 1
 
 
 def decrementer_nb_proies(grille):
     # TODO: Diminuer le compteur du nombre de proies dans la grille de 1 (Int)
-    pass
+    if grille["nb_proies"] > 0:
+        grille["nb_proies"] -= 1
 
 
 def incrementer_nb_predateurs(grille):
     # TODO: Augmenter le compteur du nombre de prédateurs dans la grille de 1 (Int)
-    pass
+    grille["nb_predateurs"] += 1
 
 
 def decrementer_nb_predateurs(grille):
     # TODO: Diminuer le compteur du nombre de prédateurs dans la grille de 1 (Int)
-    pass
+    if grille["nb_predateurs"] > 0:
+        grille["nb_predateurs"] -= 1
 
 
 def check_nb_proies(grille, max_val):
     # TODO: Vérifier si le nombre actuel de proies dans la grille est inférieur à max_val (Booléen)
-    pass
+    return grille["nb_proies"] < max_val
 
 
 def vider_case(grille, ligne, col):
     # TODO: Écraser la case située à la ligne et la colonne données avec une case vide
-    pass
+    grille["matrice"][ligne][col]["etat"] = Contenu.VIDE
+    grille["matrice"][ligne][col]["animal"] = None
 
 
-def definit_etat(grille, etat, ligne, col):
+def definir_etat(grille, etat, ligne, col):
     # TODO: Mettre à jour l'état de la case située à la ligne et la colonne données.
     # Utiliser le paramètre 'etat', qui est une valeur de l'Enum Contenu (VIDE, PROIE, PREDATEUR).
     pass
@@ -75,13 +78,19 @@ def definit_etat(grille, etat, ligne, col):
 
 def definir_animal(grille, animal, ligne, col):
     # TODO: Placer un animal (sous forme de dictionnaire) sur la case indiquée par les coordonnées (ligne, col).
-    pass
+    grille["matrice"][ligne][col]["animal"] = animal
 
 
-def obtenir_etat(grille, ligne, col):
+def obtenir_etat(grille, ligne, colonne):
     # TODO: Obtenir et retourner l'état actuel de la case à la position (ligne, col).
     # Le type de retour est une valeur de l'Enum Contenu (VIDE, PROIE, PREDATEUR).
-    pass
+    return grille["matrice"][ligne][colonne]["etat"]
+
+def obtenir_case(grille, ligne, colonne):
+    return grille["matrice"][ligne][colonne]
+
+def definir_case(grille, case, ligne, col):
+    grille["matrice"][ligne][col] = case
 
 
 def generer_entier(min_val, max_val):
@@ -90,10 +99,23 @@ def generer_entier(min_val, max_val):
     pass
 
 
+
 def ajuster_position_pour_grille_circulaire(lig, col, dim_lig, dim_col):
     # TODO: Ajuster la position (ligne, colonne) pour une grille circulaire en utilisant les dimensions de la grille.
     # Indice: Un modulo (%) peut être utile.
-    pass
+    if lig < 0:
+        lig += dim_lig
+    elif lig >= dim_lig:
+        while lig >= dim_lig:
+            lig -= dim_lig
+        
+    if col < 0:
+        col += dim_col
+    elif col >= dim_col:
+        while col >= dim_col:
+            col -= dim_col
+        
+    return lig, col
 
 
 def choix_voisin_autour(grille, ligne, col, contenu: Contenu):
@@ -101,21 +123,38 @@ def choix_voisin_autour(grille, ligne, col, contenu: Contenu):
     # TODO: Renvoyer le nombre total de ces voisins, ainsi que les coordonnées d'un voisin choisi aléatoirement (Tuple).
     #       Si le contenu n'est pas VIDE, le voisin doit être disponible (voir la fonction obtenir_disponibilite).
     # Indice: Utiliser la fonction "ajuster_position_pour_grille_circulaire" pour ajuster les positions des voisins qui sont en dehors de la grille.
-    pass
+    lig_voisin = None
+    col_voisin = None
+    tabcases = []
+    nb_ligne, nb_col = obtenir_dimensions(grille)
+    for i in range(ligne-1,ligne+2):
+        for j in range(col-1,col+2):
+            if i != ligne or j != col:
+                i2,j2 = ajuster_position_pour_grille_circulaire(i,j,nb_ligne,nb_col)
+                animal = obtenir_animal(grille, i2, j2)
+                if obtenir_etat(grille, i2, j2) == contenu and (contenu == Contenu.VIDE or animal["disponible"]):
+                    tabcases.append((i2,j2))
+    
+    if tabcases:
+        lig_voisin, col_voisin = random.choice(tabcases)
+        return len(tabcases), lig_voisin, col_voisin
+    else:
+        return len(tabcases), None, None
 
 
 def remplir_grille(grille, pourcentage_proie, pourcentage_predateur):
     # TODO: Obtenir les dimensions de la grille.
-    
+    lig, col = obtenir_dimensions(grille)
     # TODO: Calculer le nombre total de cases dans la grille.
-    
+    cases_total = lig*col
     # TODO: Calculer le nombre de proies à placer dans la grille.
-    
+    nbr_proies = int(cases_total*pourcentage_proie)
     # TODO: Calculer le nombre de prédateurs à placer dans la grille.
-    
+    nbr_predateurs = int(cases_total*pourcentage_predateur)
     # TODO: Générer et mélanger aléatoirement la liste de toutes les positions possibles.
-    
+    pos_possibles = [[i for i in range(col)]*j for j in range(lig)]
     # TODO: Placer les proies dans la grille.
+    
     # Utilisez MAX_AGE_PROIE pour générer un âge aléatoire entre 0 et l'âge maximum de la proie.
     # Utilisez NB_JRS_GESTATION_PROIE et NB_JRS_PUBERTE_PROIE pour déterminer la période de gestation si la proie est en âge de procréer.
     
