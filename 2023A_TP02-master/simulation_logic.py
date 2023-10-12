e7fde43c31632db47a7579ad591857a9042a4101
Bong <songning.lan@gmail.com>
2023-10-12 01:04:40 -0400

    fini tp2

--- 2023A_TP02-master/simulation_logic.py
+++ 2023A_TP02-master/simulation_logic.py
@@ -39,9 +39,9 @@ def executer_cycle_proie(grille, ligne, col, animal):
     if animal["age"] > MAX_AGE_PROIE:
         vider_case(grille,ligne,col)
         decrementer_nb_proies(grille)
-    elif animal["age"] >= NB_JRS_PUBERTE_PROIE and animal["jrs_gestation"] >= NB_JRS_GESTATION_PROIE:
+    elif obtenir_age(animal) >= NB_JRS_PUBERTE_PROIE and obtenir_jours_gestation(animal) >= NB_JRS_GESTATION_PROIE:
         nv,x,y = choix_voisin_autour(grille,ligne,col,Contenu.VIDE)
-        if (x and y) and grille["nb_proies"] < NB_MAX_PROIES:
+        if (x!=None and y!=None) and grille["nb_proies"] < NB_MAX_PROIES:
             case = creer_case(Contenu.PROIE,creer_animal())
             definir_case(grille,case,x,y)
             incrementer_nb_proies(grille)
@@ -56,17 +56,50 @@ def executer_cycle_predateur(grille, ligne, col, animal):
     # TODO: Gérer le cycle de vie d'un prédateur à une position donnée sur la grille.
     # 1. Vieillir l'animal. Si l'âge dépasse MAX_AGE_PRED ou si le prédateur manque d'énergie (énergie < MIN_ENERGIE), le retirer
     #    de la grille et décrémenter le compteur de prédateurs.
+    animal["age"] += NB_JRS_PUBERTE_PRED
     # 2. Si le prédateur peut manger une proie dans une case voisine, le faire en le déplaçant dans la case de la proie et en
     #    incrémentant son énergie de AJOUT_ENERGIE (n'oubliez pas de décrémenter le compteur de proies). Après avoir mangé, si le
     #    prédateur est en âge de se reproduire et a attendu suffisamment (NB_JRS_GESTATION_PRED), tenter de générer un nouveau bébé
     #    prédateur. Pour ce faire, chercher un voisin vide autour du prédateur. Si un voisin est trouvé, créer un bébé prédateur et
     #    le placer dans la grille.
+    if animal["energie"] < MIN_SANTE_PRED or animal["age"] > MAX_AGE_PRED:
+        vider_case(grille,ligne,col)
+        decrementer_nb_predateurs(grille)
+        return
+    nb_proies,x,y = choix_voisin_autour(grille,ligne,col,Contenu.PROIE)
+    if x != None and y != None:
+        definir_disponibilite(animal,False)
+        ajouter_energie(animal,AJOUT_ENERGIE)
+        definir_case(grille,creer_case(Contenu.PREDATEUR,animal),x,y)
+        vider_case(grille,ligne,col)
+        decrementer_nb_proies(grille)
+        if animal["age"] >= NB_JRS_PUBERTE_PRED and animal["jrs_gestation"] >= NB_JRS_GESTATION_PRED:
+            definir_jours_gestation(animal,0)
+            definir_animal(grille,animal,x,y)
+            a,x2,y2 = choix_voisin_autour(grille,x,y,Contenu.VIDE)
+            if x2 != None and y2 != None:
+                definir_case(grille,creer_case(Contenu.PREDATEUR,creer_animal()),x2,y2)
+                incrementer_nb_predateurs(grille)
+    else:
+        ajouter_energie(animal,-1)
+        deplacer_animal(grille,ligne,col,animal)
+
     # 3. Sinon, déplacer l'animal vers une case vide à proximité et décrémenter son énergie de 1.
-    pass
+    
 
 
 def executer_cycle(grille):
     # TODO: Marquer tous les animaux comme disponibles pour ce cycle, puis parcourir la grille pour exécuter la bonne procédure
     # du cycle de vie pour chaque animal. Il est nécessaires d'utiliser au minimum les fonctions "rendre_animaux_disponibles",
     # "executer_cycle_proie" et "executer_cycle_predateur".
-    pass
+    rendre_animaux_disponibles(grille)
+    for i in range(grille["nb_lignes"]):
+        for j in range(grille["nb_colonnes"]):
+            if obtenir_etat(grille,i,j) != Contenu.VIDE and obtenir_animal(grille,i,j) != None:
+                animal = obtenir_animal(grille,i,j)
+                if obtenir_disponibilite(animal):
+                    if obtenir_etat(grille,i,j) == Contenu.PROIE:
+                        executer_cycle_proie(grille,i,j,animal)
+                    else:
+                        executer_cycle_predateur(grille,i,j,animal)
+    
--- 2023A_TP02-master/test.py
+++ 2023A_TP02-master/test.py
@@ -6,6 +6,16 @@ from animal import *
 from grille_animaux import *
 from simulation_logic import *
 
-grille = creer_grille(3, 3)
-rendre_animaux_disponibles(grille)
-print(grille)
+	
+grille = creer_grille(5, 5)
+predateur = creer_animal(age=50, jrs_gestation=0, energie=50, disponible=True)
+case = creer_case(Contenu.PREDATEUR, predateur)
+definir_case(grille, case, 0, 0)
+proie = creer_animal(age=10, jrs_gestation=0, energie=20, disponible=True)
+case = creer_case(Contenu.PROIE, proie )
+definir_case(grille, case, 1, 0)
+incrementer_nb_proies(grille)
+incrementer_nb_predateurs(grille)
+executer_cycle_predateur(grille, 0, 0, predateur)
+population = obtenir_population(grille)
+print(population)
